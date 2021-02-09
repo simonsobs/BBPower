67c7f3520e3edde5bd288a1b98a92b94716b8b0a
Maximilian H Abitbol <mabitbol15@gmail.com>
2021-02-09 22:43:57 +0000

    cleaning and plotting

---
+++ plotting/plotter.py
@@ -0,0 +1,37 @@
+import numpy as np
+from getdist import plots, MCSamples
+from labels import alllabels, allranges
+import matplotlib as mpl
+
+
+allfs = ['/mnt/zfsusers/mabitbol/BBPower/baseline_noiseless/sim00/output0/']
+
+
+def getdist_clean(fdir):
+    sampler = np.load(fdir+'cleaned_chains.npz')
+    vals = [allranges[k] for k in sampler['labels']]
+    ranges = dict(zip(sampler['labels'], vals))
+    gd_samps = MCSamples(samples=sampler['samples'], 
+                         names=sampler['labels'], 
+                         labels=[p.strip('$') for p in sampler['labels']], 
+                         ranges=ranges)
+    return gd_samps
+
+for af in allfs: 
+    sname = af.split('/')[-3] + af.split('/')[-2]
+    outf = af+'parameter_info.txt'
+
+    samps = getdist_clean(af)
+    samps.getTable(limit=1).write(outf)
+
+    z = samps.paramNames.list()
+    g = plots.get_subplot_plotter(width_inch=16)
+    g.settings.axes_fontsize=14
+    g.settings.axes_labelsize=14
+    g.settings.line_styles = 'tab10'
+    g.triangle_plot([samps], z, shaded=True, title_limit=1)
+    mpl.pyplot.savefig('/mnt/zfsusers/mabitbol/notebooks/data_and_figures/'+sname+'_triangle')
+
+
+
+
--- plotting/saveclean.py
+++ plotting/saveclean.py
@@ -3,7 +3,50 @@ import emcee
 from labels import alllabels
 import glob
 
-allfs = ['/mnt/zfsusers/mabitbol/BBPower/moments/hybrid0chi2p3/output/']
+allfs = glob.glob('/mnt/zfsusers/mabitbol/BBPower/baseline_noiseless/sim0*/output*/')
+allfs.sort()
+
+allfs = ['/mnt/zfsusers/mabitbol/BBPower/baseline_noiseless/sim00/output0/']
+
+def next_pow_two(n):
+    i = 1
+    while i < n:
+        i = i << 1
+    return i
+
+def autocorr_func_1d(x, norm=True):
+    x = np.atleast_1d(x)
+    if len(x.shape) != 1:
+        raise ValueError("invalid dimensions for 1D autocorrelation function")
+    n = next_pow_two(len(x))
+
+    # Compute the FFT and then (from that) the auto-correlation function
+    f = np.fft.fft(x - np.mean(x), n=2 * n)
+    acf = np.fft.ifft(f * np.conjugate(f))[: len(x)].real
+    acf /= 4 * n
+
+    # Optionally normalize
+    if norm:
+        acf /= acf[0]
+
+    return acf
+
+# Automated windowing procedure following Sokal (1989)
+def auto_window(taus, c):
+    m = np.arange(len(taus)) < c * taus
+    if np.any(m):
+        return np.argmin(m)
+    return len(taus) - 1
+
+def autocorr_new(y, c=5.0):
+    f = np.zeros(y.shape[1])
+    for yy in y:
+        f += autocorr_func_1d(yy)
+    f /= len(y)
+    taus = 2.0 * np.cumsum(f) - 1.0
+    window = auto_window(taus, c)
+    return taus[window]
+
 
 
 def save_cleaned_chains(fdir):
@@ -12,38 +55,44 @@ def save_cleaned_chains(fdir):
     x = np.load(fdir+'param_chains.npz')
     labels = ['$%s$' %alllabels[k] for k in x['names']]
 
-    tau = reader.get_autocorr_time(tol=0)
-    burnin = int(10. * np.mean(tau))
-    thin = int(0.5 * np.mean(tau))
+    tau0 = reader.get_autocorr_time(tol=0)
+    burnin0 = int(5. * np.max(tau0))
+    samps = reader.get_chain(discard=burnin0)
+
+    nm = samps.shape[0]
+    nc = samps.shape[1]
+    npar = samps.shape[2]
+
+    tau = np.zeros(npar)
+    for k in range(npar):
+        tau[k] = autocorr_new(samps[:, :, k].T)
+
+    burnin = int(5. * np.max(tau))
+    thin = int(0.5 * np.max(tau))
 
     samples = reader.get_chain(discard=burnin, flat=True, thin=thin) 
-    chains = reader.get_chain(discard=burnin, flat=False) 
+    N = samples.shape[0]
 
-    N = chains.shape[0]
-    M = chains.shape[1]
-    chain_mean = np.mean(chains, axis=0)
-    chain_var = np.var(chains, axis=0)
-    samp_mean = np.mean(chains, axis=(0,1))
-    B = N / (M-1) * np.sum( (chain_mean-samp_mean)**2, axis=0 ) 
-    W = (1./M) * np.sum(chain_var, axis=0)
-    Vbar = (N-1)/N * W + (M+1)/(M*N) * B
-    GR = Vbar/W
-    
     with open(outf, 'w') as of:
-        inds = int((N/np.mean(tau)))
-        print("chains: ", chains.shape, file=of)
-        print("tau: ", np.mean(tau), np.min(tau), np.max(tau), np.std(tau), file=of)
+        inds = nm/tau
+        print("N: ", N, file=of)
         print("burnin: %d, thin: %d" %(burnin, thin), file=of)
-        print("independent samps per chain: %d" %inds, file=of)
-        print("GR", file=of)
-        print(GR, file=of)
-        if np.any(GR > 1.1):
-            print("FAILED GR", file=of)
-        if inds < 50: 
+        print("mean tau: ", np.mean(tau), file=of)
+        print("mean number independent samps per chain: %d" %np.mean(inds), file=of)
+        print("percent Mean chain uncertainty: ", 100./np.sqrt(N), file=of)
+        print("\n", file=of)
+        print("number independent samps per chain:", file=of)
+        print(inds, file=of)
+        print("tau:", file=of)
+        print(tau, file=of)
+        print("percent parameter uncertainty: ", file=of)
+        print(100./np.sqrt(inds), file=of)
+        if np.any(inds < 50): 
             print("POTENTIALLY BAD TAU", file=of)
     np.savez(fdir+'cleaned_chains', samples=samples, names=x['names'], labels=labels)
     return
 
 for af in allfs:
     save_cleaned_chains(af)
+
 
