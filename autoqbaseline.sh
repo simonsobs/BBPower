1e8ae8c301d817b05801be67e8554198eb960c6f
Maximilian H Abitbol <mabitbol15@gmail.com>
2021-02-09 18:41:41 +0000

    auto queue

--- autoqbaseline.sh
+++ autoqbaseline.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mdir="baseline_noiseless/"
+
 for k in {0..3}
 do
-    for j in {0..19}
+    for j in {0..20}
     do
-        output="sim0$k/output$j/"
+        output=$mdir"sim0$k/output$j/"
         mkdir -p $output
 
         datadir="/mnt/zfsusers/mabitbol/BBHybrid/data/sim0$k/"
@@ -13,17 +15,9 @@ do
         fid=$datadir"cls_fid_base$j.fits"
         chain=$output"param_chains.npz"
         configcopy=$output"config_copy.yml"
-        config="config.yml"
+        config=$mdir"config.yml"
 
-        addqueue -q cmb -c "2 days" -m 1 -s -n 1x12 /usr/bin/python3 -m bbpower BBCompSep   --cells_coadded=$coadd   --cells_noise=$noise   --cells_fiducial=$fid   --param_chains=$chain   --config_copy=$configcopy   --config=$config
+        addqueue -q cmb -c "2 days" -m 1 -s -n 1x8 /usr/bin/python3 -m bbpower BBCompSep   --cells_coadded=$coadd   --cells_noise=$noise   --cells_fiducial=$fid   --param_chains=$chain   --config_copy=$configcopy   --config=$config
     done
 done
 
-#--cells_coadded=/mnt/zfsusers/mabitbol/BBHybrid/data/sim00/cls_coadd_base0.fits   
-#--cells_noise=/mnt/zfsusers/mabitbol/BBHybrid/data/sim00/cls_noise_base0.fits   
-#--cells_fiducial=/mnt/zfsusers/mabitbol/BBHybrid/data/sim00/cls_fid_base0.fits   
-#--param_chains=./moments/baseline0chi2/output/param_chains.npz   
-#--config_copy=./moments/baseline0chi2/output/config_copy.yml   
-#--config=./moments/baseline0chi2/config.yml 
-
-#python3 -m bbpower BBCompSep   --cells_coadded=/mnt/zfsusers/mabitbol/BBHybrid/data/sim00/cls_coadd_base0.fits   --cells_noise=/mnt/zfsusers/mabitbol/BBHybrid/data/sim00/cls_noise_base0.fits   --cells_fiducial=/mnt/zfsusers/mabitbol/BBHybrid/data/sim00/cls_fid_base0.fits   --param_chains=./moments/baseline0chi2/output/param_chains.npz   --config_copy=./moments/baseline0chi2/output/config_copy.yml   --config=./moments/baseline0chi2/config.yml 
--- autoqresiduals.sh
+++ autoqresiduals.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mdir="residual_noiseless/"
+
 for k in {0..3}
 do
-    for j in {0..19}
+    for j in {0..20}
     do
-        output="sim0$k/output$j/"
+        output=$mdir"sim0$k/output$j/"
         mkdir -p $output
 
         cp config.yml $output
@@ -19,7 +21,7 @@ do
         chain=$output"param_chains.npz"
         configcopy=$output"config_copy.yml"
 
-        addqueue -q cmb -c "2 days" -m 1 -s -n 1x12 /usr/bin/python3 -m bbpower BBCompSep   --cells_coadded=$coadd   --cells_noise=$noise   --cells_fiducial=$fid   --param_chains=$chain   --config_copy=$configcopy   --config=$config
+        addqueue -q cmb -c "2 days" -m 1 -s -n 1x8 /usr/bin/python3 -m bbpower BBCompSep   --cells_coadded=$coadd   --cells_noise=$noise   --cells_fiducial=$fid   --param_chains=$chain   --config_copy=$configcopy   --config=$config
     done
 done
 
