08388c18fbc57b68185071de5f6f4d72cc984c4d
Kevin Wolz <kevin.wolz93@gmail.com>
2025-08-04 17:02:16 +0100

    add glamdring configs

--- jeffreys-prior/get_data.sh
+++ jeffreys-prior/get_data.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -l
 
-data_dir=/global/homes/k/kwolz/bbdev/BBPower/jeffreys-prior/data  ## YOUR DATA DIRECTORY
-
+data_dir=/mnt/extraspace/kwolz/jeffreys-prior/data  ## YOUR DATA DIRECTORY
+mkdir -p $data_dir
+cp camb_lens_nobb.dat $data_dir
+cp camb_lens_r1.dat $data_dir
 cd $data_dir
 
 wget https://portal.nersc.gov/cfs/sobs/users/so_bb/cells_coadded_all_channels_gaussian_fgs.tar.gz
