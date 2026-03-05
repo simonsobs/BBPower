3999ffb8cccae926a41e83b1dabc5030c8060cbe
kwolz <kevin.wolz93@gmail.com>
2026-03-05 14:06:34 -0500

    add tiger run examples

---
+++ examples/run_compsep_sat_wmap_planck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+## Software environment
+export OMP_NUM_THREADS=4
+export PYTHONPATH="${PYTHONPATH}:/home/kw6905/bbdev/BBPower"  # YOUR LOCAL BBPower INSTALL PATH
+
+basedir=/home/kw6905/bbdev/BBPower/examples  ## YOUR RUNNING DIRECTORY
+bbpower_dir=/home/kw6905/bbdev/BBPower  ## PATH TO YOUR LOCAL BBPOWER
+cd $basedir
+
+bbpower_config=${basedir}/config_sat_wmap_planck.yml
+python -u ${bbpower_dir}/bbpower/compsep_nopipe.py --config $bbpower_config
+python -u ${bbpower_dir}/bbpower/plotter_nopipe.py --config $bbpower_config
