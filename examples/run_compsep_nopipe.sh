d83ec456bc34f2bc1dce93d972e12ffbe109ffed
kwolz <kevin.wolz93@gmail.com>
2026-03-24 14:27:21 -0400

    rename nopipe example

--- examples/run_compsep_nopipe.sh
+++ examples/run_compsep_nopipe.sh
@@ -8,6 +8,6 @@ basedir=/home/kw6905/bbdev/BBPower/examples  ## YOUR RUNNING DIRECTORY
 bbpower_dir=/home/kw6905/bbdev/BBPower  ## PATH TO YOUR LOCAL BBPOWER
 cd $basedir
 
-bbpower_config=${basedir}/config_sat_wmap_planck.yml
+bbpower_config=${basedir}/config_nopipe.yml
 python -u ${bbpower_dir}/bbpower/compsep_nopipe.py --config $bbpower_config
 python -u ${bbpower_dir}/bbpower/plotter_nopipe.py --config $bbpower_config
