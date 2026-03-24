c40dcfeb03547eeec7ad582a50ce2724b09857d1
kwolz <kevin.wolz93@gmail.com>
2026-03-24 14:10:37 -0400

    clean up tests, delete unused ones

--- test/run_polychord_test.sh
+++ test/run_polychord_test.sh
@@ -5,16 +5,16 @@ mkdir -p test/test_out
 # Generate fiducial cls
 python ./examples/generate_SO_spectra.py test/test_out
 
-# Generate simulations
-for seed in {1001..1100}
-do
-   mkdir -p test/test_out/s${seed}
-   echo ${seed}
-   python examples/generate_SO_maps.py --output-dir test/test_out/s${seed} --seed ${seed} --nside 64
-done
+# # Generate simulations
+# for seed in {1001..1005}
+# do
+#    mkdir -p test/test_out/s${seed}
+#    echo ${seed}
+#    python examples/generate_SO_maps.py --output-dir test/test_out/s${seed} --seed ${seed} --nside 64
+# done
 
 # Run pipeline
-python -m bbpower BBPowerSpecter   --splits_list=./examples/test_data/splits_list.txt   --masks_apodized=./examples/data/maps/norm_nHits_SA_35FOV.fits   --bandpasses_list=./examples/data/bpass_list.txt   --sims_list=./examples/test_data/sims_list.txt   --beams_list=./examples/data/beams_list.txt   --cells_all_splits=./test/test_out/cells_all_splits.fits   --cells_all_sims=./test/test_out/cells_all_sims.txt   --mcm=./test/test_out/mcm.dum   --config=./test/test_config_polychord.yml
+python -m bbpower BBPowerSpecter   --splits_list=./examples/test_data/splits_list.txt   --masks_apodized=./examples/data/maps/norm_nHits_SA_35FOV.fits   --bandpasses_list=./examples/data/bpass_list.txt   --sims_list=./examples/test_data/sims_list_10.txt   --beams_list=./examples/data/beams_list.txt   --cells_all_splits=./test/test_out/cells_all_splits.fits   --cells_all_sims=./test/test_out/cells_all_sims.txt   --mcm=./test/test_out/mcm.dum   --config=./test/test_config_polychord.yml
 
 python -m bbpower BBPowerSummarizer   --splits_list=./examples/test_data/splits_list.txt   --bandpasses_list=./examples/data/bpass_list.txt   --cells_all_splits=./test/test_out/cells_all_splits.fits   --cells_all_sims=./test/test_out/cells_all_sims.txt   --cells_coadded_total=./test/test_out/cells_coadded_total.fits   --cells_coadded=./test/test_out/cells_coadded.fits   --cells_noise=./test/test_out/cells_noise.fits   --cells_null=./test/test_out/cells_null.fits   --config=./test/test_config_polychord.yml
 
@@ -29,5 +29,5 @@ else
     echo "Test passed"
 fi
 
-# Cleanup
-rm -r test/test_out
+# # Cleanup
+# rm -r test/test_out
